9dc645503ebb2bb908b622a913500a693ce1eca6
kinderswan <gamanovichpavel@gmail.com>
2017-06-14 21:31:33 +0300

    initial commit

---
+++ tamriel/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TamrielPage } from './app.po';
+
+describe('tamriel App', () => {
+  let page: TamrielPage;
+
+  beforeEach(() => {
+    page = new TamrielPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
